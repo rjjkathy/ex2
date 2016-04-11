10fd7b2061eed5785996e5bece301f3fec35f4dd
Jingjing <rjj@berkeley.edu>
2016-04-11 02:08:18 -0700

    syncing some edits done in aws instance

--- EX2Tweetwordcount/createDBAndTable.py
+++ EX2Tweetwordcount/createDBAndTable.py
@@ -4,9 +4,9 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
  
 # DB Tcount
 conn = psycopg2.connect(database="postgres", user="postgres", password="pass", host="localhost", port="5432") 
-#conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT) 
+conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT) 
 cur = conn.cursor() 
-cur.execute('DROP DATABASE IF EXISTS Tcount') 
+cur.execute("DROP DATABASE IF EXISTS Tcount") 
 conn.commit() 
 cur.execute("CREATE DATABASE Tcount") 
 conn.commit() 
